52144c00b4785ddea66d93d075de20333580b8d6
zhenming wang <zhenmingwang@dhcp-10-105-115-137.wireless.northwestern.private>
2022-09-28 16:44:41 -0500

    style

--- src/components/CourseItem.jsx
+++ src/components/CourseItem.jsx
@@ -4,7 +4,13 @@ import './CourseItem.css';
 class CourseItem extends React.Component{
     render(){
         let data = this.props.data;
-        return <li>{`${data[1].term}:CS ${data[1].number}  ${data[1].title}`}</li>
+        // term+num, title, time
+        console.log(data);
+        return <li className= "course-card">
+            <div className="course-head course">{`${data[1].term} CS ${data[1].number} `}</div>
+            <div className='course-body course'>{data[1].title}</div>
+            <div className="course-foot course">{data[1].meets}</div>
+        </li>
     }
 }
 
