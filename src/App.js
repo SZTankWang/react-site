1843fa8d3bbaa377c6864586495e46a0d09b962f
zhenming wang <zhenmingwang@zhenmingdembp.ads.northwestern.edu>
2022-10-27 11:51:47 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import  { useState,useEffect } from 'react';
-import { Doquery } from './utilities/fetch';
+import { getData} from './utilities/fetch';
 
 
 function App() {
+  const [courses,setCourses] = useState(null);
   const [called,setCalled] = useState(false);
   const [term, setTerm] = useState("Fall");
   // const [courses,setCourses] = useState([]);
@@ -23,40 +24,57 @@ function App() {
     console.log("parsed:",parsed);
   },[parsed]);
 
+  useEffect(()=>{
+    const data =getData();
+    data.then((rsp)=>{
+      console.log(rsp);
+      setCourses(rsp);
+    })
+  },[])
 
-  let response = Doquery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+  // let response = Doquery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
   // console.log("response:",response);
 
+  //get from firebase
+  
+  // console.log(data);
+  let course_list;
+  if(courses){
+    course_list = <CourseList data={courses} term={term} selected={selected} setSelect={setSelect} parsed={parsed} setParsed={setParsed}></CourseList>
 
+  }
+  else{
+    course_list = "loading data"
+  }
+    return (
+      <div className="App">
+        <Banner term={term} setTerm={setTerm} selected={selected}></Banner>
+        {course_list}
+
+      </div>
+    );
+    
 
 
     
   
-    if(response.isLoading){
-        return  <div className="App">
-        {/* <Banner term={term} setTerm={setTerm}></Banner> */}
-        <h1>Loading data</h1>
-        </div>
-    }
+    // if(response.isLoading){
+    //     return  <div className="App">
+    //     {/* <Banner term={term} setTerm={setTerm}></Banner> */}
+    //     <h1>Loading data</h1>
+    //     </div>
+    // }
   
-    if(response.error){
-        // console.log(response.error.message) ;
-    }
-    if(response.data){
+    // if(response.error){
+    //     // console.log(response.error.message) ;
+    // }
+
       // setData(response.data);
       // setCalled(true);
       // console.log(response.data);
-      return (
-        <div className="App">
-          <Banner term={term} setTerm={setTerm} selected={selected}></Banner>
-    
-          <CourseList data={response.data} term={term} selected={selected} setSelect={setSelect} parsed={parsed} setParsed={setParsed}></CourseList>
-    
-          
-        </div>
-      );
+
   
-    }
+    
   
 
 
--- src/utilities/fetch.js
+++ src/utilities/fetch.js
@@ -1,21 +1,60 @@
 import {useQuery} from "@tanstack/react-query";
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, child, get } from "firebase/database";
 
-const querydata = async (url) =>{
-    return await fetch(url)
-    .then((rsp) => {
-        if(!rsp.ok){
-            throw new Error("network response not ok");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgrf1V3iGvSd3R6qqxIPaEx9H2__lLZRA",
+    authDomain: "wp2ws-week10.firebaseapp.com",
+    databaseURL: "https://wp2ws-week10-default-rtdb.firebaseio.com",
+    projectId: "wp2ws-week10",
+    storageBucket: "wp2ws-week10.appspot.com",
+    messagingSenderId: "376710954688",
+    appId: "1:376710954688:web:609fc4d89c6ce40dd57725",
+    measurementId: "G-H839LQKZTG"
+  };
+
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+const dbRef = ref(database);
+
+
+export const getData = ()=>{
+    return get(child(dbRef, `course-data`)).then((snapshot) => {
+        if (snapshot.exists()) {
+        //   console.log(snapshot.val());
+          return snapshot.val();
+        } else {
+          console.log("No data available");
         }
-        return rsp.json();
-
-    })
-
+      }).catch((error) => {
+        console.error(error);
+      });
+      
 }
 
-export const Doquery = (url) => {
+
+
+// const querydata = async (url) =>{
+//     return await fetch(url)
+//     .then((rsp) => {
+//         if(!rsp.ok){
+//             throw new Error("network response not ok");
+//         }
+//         return rsp.json();
+
+//     })
+
+// }
+
+// export const Doquery = (url) => {
     
-        const {isLoading,isError,data,error} = useQuery(["courses"],()=> querydata(url));
-        return {data,isLoading,error};
+//         const {isLoading,isError,data,error} = useQuery(["courses"],()=> querydata(url));
+//         return {data,isLoading,error};
     
 
-}
+// }
